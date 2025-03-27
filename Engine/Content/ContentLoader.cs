dad491db8dc7602709330b405adf314577a08422
Samuel Zwijsen <70766878+swzwij@users.noreply.github.com>
2025-03-27 15:35:42 +0100

    Renamed states, and added caching to content loader

--- Engine/Content/ContentLoader.cs
+++ Engine/Content/ContentLoader.cs
@@ -1,19 +1,32 @@
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Content;
 
-namespace MonoEight
+namespace MonoEight;
+
+public static class ContentLoader
 {
-    public class ContentLoader
+    private static ContentManager _content;
+    private static readonly Dictionary<string, object> _contentCache = [];
+
+    public static void Initialize(ContentManager content)
     {
-        private static ContentManager _content;
+        _content = content;
+    }
 
-        public static void Initialize(ContentManager content)
-        {
-            _content = content;
-        }
+    public static T Load<T>(string path)
+    {
+        if(_contentCache.TryGetValue(path, out object content) && content is T typedContent)
+            return typedContent;
 
-        public static T Load<T>(string assetName)
-        {
-            return _content.Load<T>(assetName);
-        }
+        T loadedContent = _content.Load<T>(path);
+        return Cache(path, loadedContent);
+    }
+
+    public static T Cache<T>(string name, T content)
+    {
+        if (_contentCache.TryAdd(name, content))
+            return content;
+
+        throw new System.ArgumentException($"Content '{name}' of type {typeof(T).Name} already exists in the loader");
     }
 }
