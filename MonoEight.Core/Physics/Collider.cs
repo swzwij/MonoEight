622fc1c2e8dba44ee074446c8b33db581b61347a
swzwij <swzwij@gmail.com>
2026-01-10 03:22:26 +0100

    Started collision documentation

--- MonoEight.Core/Physics/Collider.cs
+++ MonoEight.Core/Physics/Collider.cs
@@ -2,12 +2,37 @@
 
 namespace MonoEight.Core.Physics
 {
+    /// <summary>
+    /// A base collider class to handle intersection.
+    /// </summary>
     public abstract class Collider : Component
     {
+        /// <summary>
+        /// Whether this collider intersects with the other given collider. 
+        /// </summary>
+        /// <param name="other">The other collider used to check intersection.</param>
+        /// <returns><c>true</c> if the colliders intersect.</returns>
         public abstract bool Intersects(Collider other);
-
+        
+        /// <summary>
+        /// Whether this collider intersects with the given <see cref="Point"/>.
+        /// </summary>
+        /// <param name="point">The <see cref="Point"/> to check against.</param>
+        /// <returns><c>true</c> if the collider intersect with the given <see cref="Point"/>.</returns>
         public abstract bool Intersects(Point point);
+        
+        /// <summary>
+        /// Whether this collider intersects with the given <see cref="BoxCollider"/>.
+        /// </summary>
+        /// <param name="other">The <see cref="BoxCollider"/> to check against.</param>
+        /// <returns><c>true</c> if the collider intersect with the given <see cref="BoxCollider"/>.</returns>
         public abstract bool Intersects(BoxCollider other);
+        
+        /// <summary>
+        /// Whether this collider intersects with the given <see cref="CircleCollider"/>.
+        /// </summary>
+        /// <param name="other">The <see cref="CircleCollider"/> to check against.</param>
+        /// <returns><c>true</c> if the collider intersect with the given <see cref="CircleCollider"/>.</returns>
         public abstract bool Intersects(CircleCollider other);
     }
 }
--- MonoEight.Core/Physics/CollisionHelper.cs
+++ MonoEight.Core/Physics/CollisionHelper.cs
@@ -2,8 +2,17 @@
 
 namespace MonoEight.Core.Physics
 {
+    /// <summary>
+    /// Provides helper methods for calculating if colliders are intersecting.
+    /// </summary>
     public static class CollisionHelper
     {
+        /// <summary>
+        /// Whether the given <see cref="CircleCollider"/> and <see cref="BoxCollider"/> intersect.
+        /// </summary>
+        /// <param name="circle">The <see cref="CircleCollider"/>.</param>
+        /// <param name="box">The <see cref="BoxCollider"/>.</param>
+        /// <returns><c>trye</c> if the given <see cref="Collider"/> intersect.</returns>
         public static bool CircleBox(CircleCollider circle, BoxCollider box)
         {
             float closestX = Math.Clamp(circle.Position.X, box.Min.X, box.Max.X);
@@ -15,12 +24,24 @@ namespace MonoEight.Core.Physics
             return distanceSquared < circle.Radius * circle.Radius;
         }
 
+        /// <summary>
+        /// Whether the given <see cref="Point"/> and <see cref="BoxCollider"/> intersect.
+        /// </summary>
+        /// <param name="point">The <see cref="Point"/>.</param>
+        /// <param name="box">The <see cref="BoxCollider"/>.</param>
+        /// <returns><c>true</c> if the given <see cref="Collider"/> and <see cref="Point"/> intersect.</returns>
         public static bool PointBox(Point point, BoxCollider box)
         {
             return point.X >= box.Min.X && point.X <= box.Max.X &&
                    point.Y >= box.Min.Y && point.Y <= box.Max.Y;
         }
 
+        /// <summary>
+        /// Whether a given <see cref="Point"/> and <see cref="CircleCollider"/> intersect.
+        /// </summary>
+        /// <param name="point">The <see cref="Point"/>.</param>
+        /// <param name="circle">The <see cref="CircleCollider"/>.</param>
+        /// <returns><c>true</c> if the given <see cref="Collider"/> and <see cref="Point"/> intersect.</returns>
         public static bool PointCircle(Point point, CircleCollider circle)
         {
             float dx = point.X - circle.Position.X;
@@ -29,6 +50,12 @@ namespace MonoEight.Core.Physics
             return distanceSquared < circle.Radius * circle.Radius;
         }
 
+        /// <summary>
+        /// Whether the two given <see cref="CircleCollider">CircleColliders</see> intersect.
+        /// </summary>
+        /// <param name="a">The first <see cref="CircleCollider"/>.</param>
+        /// <param name="b">The second <see cref="CircleCollider"/>.</param>
+        /// <returns><c>true</c> if the given <see cref="CircleCollider">CircleColliders</see> intersect.</returns>
         public static bool CircleCircle(CircleCollider a, CircleCollider b)
         {
             float dx = a.Position.X - b.Position.X;
@@ -38,6 +65,12 @@ namespace MonoEight.Core.Physics
             return distanceSquared < radiusSum * radiusSum;
         }
 
+        /// <summary>
+        /// Whether the two given <see cref="BoxCollider">BoxColliders</see> intersect.
+        /// </summary>
+        /// <param name="a">The first <see cref="BoxCollider"/>.</param>
+        /// <param name="b">The second <see cref="BoxCollider"/>.</param>
+        /// <returns><c>true</c> if the given <see cref="BoxCollider">BoxColliders</see> intersect.</returns>
         public static bool BoxBox(BoxCollider a, BoxCollider b)
         {
             return a.Min.X < b.Max.X &&
