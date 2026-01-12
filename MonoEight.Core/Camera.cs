f43b872b5c68d1277eb7731f1dd04be89bf53f93
swzwij <swzwij@gmail.com>
2026-01-12 21:20:02 +0100

    Added docs to camera class

--- MonoEight.Core/Camera.cs
+++ MonoEight.Core/Camera.cs
@@ -2,25 +2,50 @@ using Microsoft.Xna.Framework;
 
 namespace MonoEight.Core;
 
+/// <summary>
+/// Represents a 2D camera that defines the viewable area of the game world.
+/// </summary>
+/// <remarks>
+/// The camera centers the view on a given world position.
+/// </remarks>
 public class Camera
 {
-    private Matrix _transform;
     private Vector2 _position;
-
+    
+    /// <summary>
+    /// Gets or sets the background color used when clearing the screen.
+    /// Default is <see cref="MEColors.Blue"/>.
+    /// </summary>
     public Color BackgroundColor { get; set; }
+    
+    /// <summary>
+    /// Gets the transformation matrix used for rendering.
+    /// </summary>
+    public Matrix Transform { get; private set; }
 
-    public Matrix Transform => _transform;
-
+    /// <summary>
+    /// Gets or sets the world position that the camera is at.
+    /// </summary>
+    /// <remarks>
+    /// Setting this value automatically recalculates the <see cref="Transform"/> matrix.
+    /// </remarks>
     public Vector2 Position
     {
-        get => _position + new Vector2(MEWindow.Resolution.X / 2 , MEWindow.Resolution.Y / 2);
+        get => _position + new Vector2(MEWindow.Resolution.X / 2f, MEWindow.Resolution.Y / 2f);
         set
         {
-            _position = new(value.X - MEWindow.Resolution.X / 2, value.Y - MEWindow.Resolution.Y / 2);
+            _position = new Vector2
+            (
+                value.X - MEWindow.Resolution.X / 2f, 
+                value.Y - MEWindow.Resolution.Y / 2f
+            );
             UpdatePosition();
         }
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Camera"/> class centered at (0,0).
+    /// </summary>
     public Camera()
     {
         Position = Vector2.Zero;
@@ -29,6 +54,6 @@ public class Camera
 
     private void UpdatePosition()
     {
-        _transform = Matrix.CreateTranslation((int)-_position.X, (int)-_position.Y, 0);
+        Transform = Matrix.CreateTranslation((int)-_position.X, (int)-_position.Y, 0);
     }
 }
